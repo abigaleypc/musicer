7294425d3ce34499e364791c5f866dc9522920cd
Yu Abigale <abigaleypc@gmail.com>
2017-12-14 16:17:10 +0800

    build

--- public/main.js
+++ public/main.js
@@ -69,3 +69,17 @@ const { ipcMain } = require('electron')
 ipcMain.on('login-event', (event, arg) => {
   mainWindow.webContents.send('login-event', arg);
 })
+
+const express = require('express');
+const {user, test, song} = require('../src/routes');
+
+const PORT = process.env.PORT || 8082;
+const server = express();
+
+server.use('/user', user);
+server.use('/test', test);
+server.use('/song', song);
+
+server.listen(PORT, () => {
+  console.log(`The server has been set up at 0.0.0.0:${PORT}`);
+});
